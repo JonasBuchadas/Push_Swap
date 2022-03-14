42ba3b698a317a3d03d7b9f2e403a75cca291940
JonasBuchadas <jonasmcbuchadas@gmail.com>
2022-03-14 12:52:05 +0000

    Simplified Push_median function

--- Push_Swap/push_median.c
+++ Push_Swap/push_median.c
@@ -1,20 +1,17 @@
 #include "push_swap.h"
 
 static void	check_sender_stack_median(t_dlist *a, int median, t_s *inf);
-static int	choose_moves(t_s *i_s);
-static void	execute_moves_median(t_dlist **a, t_dlist **b, int m);
+static void	execute_moves_median(t_dlist **a, t_dlist **b, t_s *i_s);
 
 void	push_median(t_dlist **s, t_dlist **r, t_s *inf_s)
 {	
 	int	median;
-	int	m;
 
 	median = inf_s->median;
 	while (inf_s->min < median)
 	{	
 		check_sender_stack_median(*s, median, inf_s);
-		m = choose_moves(inf_s);
-		execute_moves_median(s, r, m);
+		execute_moves_median(s, r, inf_s);
 		stack_info(*s, inf_s);
 	}
 }
@@ -37,18 +34,11 @@ static void	check_sender_stack_median(t_dlist *s, int median, t_s *inf)
 	inf->r_num = *(int *)temp->content;
 }
 
-static int	choose_moves(t_s *i_s)
+static void	execute_moves_median(t_dlist **a, t_dlist **b, t_s *i_s)
 {
 	if (i_s->rot == 0)
-		return (0);
-	else
-		return (1);
-}
-
-static void	execute_moves_median(t_dlist **a, t_dlist **b, int m)
-{
-	if (m == 0)
 		pb(a, b, true);
-	else if (m == 1)
+	else
 		ra(a, true);
-}
+}	
+
